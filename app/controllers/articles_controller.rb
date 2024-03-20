e71dce8b37fe9b8520359ecbde56c591de69e2a0
light-planck <seseifunc16@gmail.com>
2024-03-20 14:31:41 +0900

    feat: article詳細ページの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@
 
 class ArticlesController < ApplicationController
   def index
-    @artciles = Article.all
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   root 'articles#index'
+
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
