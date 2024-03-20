9118527a0faf85bae4d97928929226f54822a6f2
light-planck <seseifunc16@gmail.com>
2024-03-20 15:17:46 +0900

    refactor: route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  resources :articles
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
